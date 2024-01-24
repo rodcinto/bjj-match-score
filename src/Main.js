e310f1349c7c22a59f1fb772f7e6b6ba4ff3c5a1
Rodrigo Cinto <rodcinto@gmail.com>
2024-01-24 17:08:09 +0100

    Adding first theme.

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
+import {
+  PaperProvider,
+  MD3LightTheme as DefaultTheme
+} from 'react-native-paper';
 import { name as appName } from './app.json';
 import App from './App';
 
+import light from './themes/redAndBlue/light.json'
+
+const theme = {
+  ...DefaultTheme,
+  colors: light.colors,
+};
+
 export default function Main() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <App />
     </PaperProvider>
   );
