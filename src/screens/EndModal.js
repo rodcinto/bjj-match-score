67764a36e114239b82aec45ccda0d4a2aec5b337
Rodrigo Cinto <rodcinto@gmail.com>
2024-01-24 16:18:00 +0100

    EndModal appearence enhancements.

--- src/screens/EndModal.js
+++ src/screens/EndModal.js
@@ -2,13 +2,31 @@ import { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Modal, Text, ActivityIndicator } from 'react-native-paper';
 
-function ParticipantDetails({ name, points }) {
+const blueCornerTxtColor = 'rgb(78, 106, 249)';
+const redCornerTxtColor = 'rgb(253, 70, 70)';
+const redCornerBgColor = 'rgba(255, 215, 195, 1)';
+const blueCornerBgColor = 'rgba(230, 234, 255, 1)';
+
+function defineBgColor(winner) {
+  if (!winner) {
+    return 'white';
+  }
+
+  return winner.key === 'BLUE' ? blueCornerBgColor : redCornerBgColor
+}
+
+function defineNameColor(corner) {
+  return corner === 'BLUE' ? blueCornerTxtColor : redCornerTxtColor
+};
+
+function ParticipantDetails({ name, points, corner }) {
+
   return (
-    <View>
-      <Text>{name}</Text>
-      <Text>{points.rawPoints} points</Text>
-      <Text>{points.advantages} Advantages</Text>
-      <Text>{points.penalties} Penalties</Text>
+    <View style={styles.detailsSet}>
+      <Text variant='titleLarge' style={[{color: defineNameColor(corner)}]}>{name}</Text>
+      <Text variant='bodyMedium'>{points.rawPoints} points</Text>
+      <Text variant='bodyMedium'>{points.advantages} Advantages</Text>
+      <Text variant='bodyMedium'>{points.penalties} Penalties</Text>
     </View>
   );
 }
@@ -50,29 +68,35 @@ export default function EndModal({ visible, onDismiss, report }) {
   }, [visible]);
 
   return (
-    <Modal visible={visible} onDismiss={onDismiss} contentContainerStyle={styles.container}>
-      <Text variant="displaySmall">Match End</Text>
+    <Modal visible={visible} onDismiss={onDismiss} contentContainerStyle={[styles.container, {backgroundColor: defineBgColor(winner)}]}>
+      <Text variant="displaySmall" style={styles.headlineText}>Match End</Text>
       {isObjectEmpty(report) ?
         (<ActivityIndicator />)
         :
         (winner ?
           (
             <View>
-              <View>
-                <Text>Winner: {winner.name} by {reason}</Text>
-                <Text>{winner.points.rawPoints} points</Text>
+              <View style={styles.winnerWrapper}>
+                <Text variant='headlineMedium' style={styles.winnerTxt}>
+                  <Text style={{color: defineNameColor(winner.key)}}>{winner.name}</Text> win by {reason}
+                </Text>
+                <Text variant='headlineSmall' style={styles.winnerPts}>{winner.points.rawPoints} pts.</Text>
               </View>
-              <ParticipantDetails name={winner.name} points={winner.points} />
-              <ParticipantDetails name={defeated.name} points={defeated.points} />
+              <View style={styles.detailsWrapper}>
+                <ParticipantDetails name={winner.name} points={winner.points} corner={winner.key} />
+                <ParticipantDetails name={defeated.name} points={defeated.points} corner={defeated.key} />
+              </View>
             </View>
           ) :
           (
             <View>
               <View>
-                <Text>DRAW</Text>
+                <Text variant='headlineLarge' style={styles.drawTxt}>DRAW</Text>
               </View>
-              <ParticipantDetails name={report.P1.name} points={report.P1.points} />
-              <ParticipantDetails name={report.P2.name} points={report.P2.points} />
+              <View style={styles.detailsWrapper}>
+                <ParticipantDetails name={report.P1.name} points={report.P1.points} corner={report.P1.key} />
+                <ParticipantDetails name={report.P2.name} points={report.P2.points} corner={report.P2.key} />
+              </View>
             </View>
           ))
       }
@@ -84,6 +108,34 @@ export default function EndModal({ visible, onDismiss, report }) {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
-    padding: 20,
+    padding: 50,
+  },
+  headlineText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    marginBottom: 50,
+  },
+  winnerWrapper: {},
+  winnerTxt: {
+    textAlign: 'center',
+  },
+  winnerPts: {
+    textAlign: 'center',
+  },
+  detailsWrapper: {
+    flexDirection: 'row',
+    marginTop: 50,
+  },
+  detailsSet: {
+    flexDirection: 'column',
+    width: '50%',
+    alignItems: 'center',
+  },
+  drawTxt: {
+    color: 'slategray',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontStyle: 'italic',
   }
 });
