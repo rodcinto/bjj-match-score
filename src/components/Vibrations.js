053d5af72fc78f1c2891b3290652090438a8708b
Rodrigo Cinto <rodcinto@gmail.com>
2024-01-24 10:46:51 +0100

    Vibrations.

---
+++ src/components/Vibrations.js
@@ -0,0 +1,47 @@
+import { Vibration } from "react-native";
+
+import vibrationMap from "../domain/vibrationMap";
+
+export default class Vibrations {
+  static byPoints(points) {
+    switch(points) {
+      case 2:
+        Vibration.vibrate(vibrationMap['two']);
+        break;
+      case 3:
+        Vibration.vibrate(vibrationMap['three']);
+        break;
+      case 4:
+        Vibration.vibrate(vibrationMap['four']);
+        break;
+    }
+  }
+
+  static endGame() {
+    Vibration.vibrate(vibrationMap['endGame']);
+  }
+
+  static vibrateDefault() {
+    Vibration.vibrate(vibrationMap['default']);
+  }
+
+  static advantage() {
+    Vibration.vibrate(vibrationMap['adv']);
+  }
+
+  static penalty() {
+    Vibration.vibrate(vibrationMap['pnlt']);
+  }
+
+  static undo() {
+    Vibration.vibrate(vibrationMap['undo']);
+  }
+
+  static playPause() {
+    Vibration.vibrate(vibrationMap['playPause']);
+  }
+
+  static finish() {
+    Vibration.vibrate(vibrationMap['finish']);
+  }
+}
---
+++ src/domain/vibrationMap.js
@@ -0,0 +1,17 @@
+const vibrationMap = [];
+const DEFAULT = [0, 100];
+
+// Alphabetic order, please :)
+vibrationMap['adv'] = [0, 200];
+vibrationMap['default'] = DEFAULT;
+vibrationMap['endGame'] = [0, 200, 70, 200, 50 ,100];
+vibrationMap['finish'] = [0, 100, 50, 200];
+vibrationMap['four'] = [0, 100, 50, 100, 50 ,100, 50 , 100];
+vibrationMap['matchEnd'] = [0, 100, 50, 200];
+vibrationMap['playPause'] = DEFAULT;
+vibrationMap['pnlt'] = [0, 100, 50, 100, 50 ,200];
+vibrationMap['three'] = [0, 100, 50, 100, 50 ,100];
+vibrationMap['two'] = [0, 100, 50, 100];
+vibrationMap['undo'] = DEFAULT;
+
+export default vibrationMap;
