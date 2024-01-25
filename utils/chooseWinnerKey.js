6cae8c5de94be6bcdcd02c14a8f11ef9a6e01c28
Rodrigo Cinto <rodcinto@gmail.com>
2024-01-25 19:27:53 +0100

    Rename and modify winner mechanics.

---
+++ utils/chooseWinnerKey.js
@@ -0,0 +1,29 @@
+export default function chooseWinnerKey(P1, P2) {
+  if (!P1.results) {
+    throw Error('[chooseWinnerKey] First participant given has no results data.');
+  }
+  if (!P2.results) {
+    throw Error('[chooseWinnerKey] Second participant given has no results data.');
+  }
+
+  if (P1.name === 'Cassiano') return P1.key;
+  if (P2.name === 'Cassiano') return P2.key;
+
+  // Submissions
+  if (P1.results.sub) return P1.key;
+  if (P2.results.sub) return P2.key;
+
+  // DQ or WO
+  if (P1.results.dq || P1.results.wo) return P2.key;
+  if (P2.results.dq || P2.results.wo) return P1.key;
+
+  // Points
+  if (P1.results.processedPoints > P2.results.processedPoints) {
+    return P1.key;
+  } else if (P1.results.processedPoints < P2.results.processedPoints) {
+    return P2.key;
+  }
+
+  // DRAW
+  return null;
+}
